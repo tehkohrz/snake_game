27e81f091fd55336dabce5782b0c9ab6ee04a59b
tehkohrz <dongkunkoh@hotmail.com>
2022-07-03 16:52:32 +0800

    Chore: Added webpack config for prodcution

---
+++ webpack.prod.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // This is for the changing webpacked version, generate a new index.html file to link with the new index.js
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  entry: './src/initApp.js',
+  output: {
+    filename: 'snakeGame.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
